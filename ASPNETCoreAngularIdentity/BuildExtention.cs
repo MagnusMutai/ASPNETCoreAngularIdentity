cef2454c31daab13d90f4fb73e3fb78f27d9b7fa
Magnus Mutai <MagnusMq07@gmail.com>
2024-07-13 12:02:04 +0300

    Added angular project with aspnetcore project

---
+++ ASPNETCoreAngularIdentity/BuildExtention.cs
@@ -0,0 +1,26 @@
+public static class BuildExtension
+{
+public static WebApplication BuildWithSpa(this WebApplicationBuilder builder)
+{
+    var app = builder.Build();
+
+    app.UseRouting();
+
+    app.UseEndpoints(_ => { });
+
+    app.Use((ctx, next) =>
+    {
+        if (ctx.Request.Path.StartsWithSegments("/api"))
+        {
+            ctx.Response.StatusCode = 404;
+            return Task.CompletedTask;
+        }
+
+        return next();
+    });
+
+    app.UseSpa(x => x.UseProxyToSpaDevelopmentServer());
+
+    return app
+}
+}
