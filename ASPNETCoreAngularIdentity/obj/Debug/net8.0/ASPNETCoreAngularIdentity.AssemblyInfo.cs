a95ae27970ecdb03ea935432101a2bdd30343144
Magnus Mutai <MagnusMq07@gmail.com>
2024-08-31 10:34:32 +0300

    Removed old Angular project and added a new generic Angular project

--- ASPNETCoreAngularIdentity/obj/Debug/net8.0/ASPNETCoreAngularIdentity.AssemblyInfo.cs
+++ ASPNETCoreAngularIdentity/obj/Debug/net8.0/ASPNETCoreAngularIdentity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPNETCoreAngularIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4d6de3b2aeba8043e43748ab3b385c224ac650")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db06f18c3e296f04fc43db671b085a6df1bb198")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPNETCoreAngularIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPNETCoreAngularIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
